73ba059f6227498003c1a44d778bd7fd8aad6152
Michał Rączka <me@michaloo.net>
2015-01-02 20:58:37 +0100

    Add date padding, person forms linking

--- src/lib/Datify.php
+++ src/lib/Datify.php
@@ -5,13 +5,29 @@ namespace Resform\Lib;
 class Datify extends \Sirius\Filtration\Filter\AbstractFilter
 {
 
+    function pad($string, $length) {
+        return str_pad($string, $length, "0", STR_PAD_LEFT);
+    }
+
     function filterSingle($value, $valueIdentifier = null) {
 
         $filtered = trim($value);
 
         $filtered = str_replace(" ", "-", $filtered);
 
-        return $filtered;
+        if (substr_count($filtered, "-") != 2) {
+            return null;
+        }
+
+        $tmp = explode("-", $filtered);
+
+        $filtered = array(
+            $this->pad($tmp[0], 2),
+            $this->pad($tmp[1], 2),
+            $this->pad($tmp[2], 4)
+        );
+
+        return join("-", $filtered);
     }
 
 
